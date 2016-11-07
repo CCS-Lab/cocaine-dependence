a7e62d11773a36e70b8611ee3d71317ea36f28a9
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-06 20:23:41 -0500

    updated analysis to have dataset internally

--- Python/analysis.py
+++ Python/analysis.py
@@ -1,5 +1,4 @@
 import matplotlib as mpl
-import pandas as pd
 
 # Set matplotlib settings
 mpl.get_backend()
@@ -7,12 +6,13 @@ mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
+from easyml.datasets import cocaine
 from easyml.factory import easy_glmnet
 
 
 if __name__ == "__main__":
     # Load data
-    data = pd.read_table('../data/cocaine.txt')
+    data = cocaine.load_data()
 
     # Analyze data
     easy_glmnet(data, dependent_variable='DIAGNOSIS', family='binomial', exclude_variables=['subject', 'AGE'])
