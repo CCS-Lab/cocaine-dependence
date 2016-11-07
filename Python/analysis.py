579d6a23e2687539944e3211a8fea90ef782018f
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-06 20:14:14 -0500

    updated analysis file

---
+++ Python/analysis.py
@@ -0,0 +1,18 @@
+import matplotlib as mpl
+import pandas as pd
+
+# Set matplotlib settings
+mpl.get_backend()
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+from easyml.factory import easy_glmnet
+
+
+if __name__ == "__main__":
+    # Load data
+    data = pd.read_table('../data/cocaine.txt')
+
+    # Analyze data
+    easy_glmnet(data, dependent_variable='DIAGNOSIS', family='binomial', exclude_variables=['subject', 'AGE'])
--- Python/scripts/setup_osx.sh
+++ Python/scripts/setup_osx.sh
@@ -3,4 +3,4 @@
 echo "Creating conda environment..."
 conda env create -f environment.yml
 
-echo "Conda environment created! Make sure to run \`source activate cocaine-dependence\` whenever you open a new terminal and want to run programs under cocaine-dependence."
+echo "Conda environment created!"
