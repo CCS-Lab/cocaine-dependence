a6f20080dc8d21ccf2b64f2d2acc58696712920f
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-10 18:41:17 -0500

    updated R analysis file

--- R/analysis.R
+++ R/analysis.R
@@ -1,5 +1,8 @@
 library(easyml)
 
+# Load data
 data("cocaine", package = "easyml")
+
+# Analyze data
 easy_glmnet(data = cocaine, dependent_variable = "DIAGNOSIS", family = "binomial",
             exclude_variables = c("subject", "AGE"))
