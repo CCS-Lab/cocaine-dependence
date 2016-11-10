33d7d6aab3c2316b8ee107e00ca2ed6e8c9a6af5
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-10 18:42:13 -0500

    updated python analysis file and environment file

--- Python/analysis.py
+++ Python/analysis.py
@@ -1,4 +1,5 @@
 import matplotlib as mpl
+import pandas as pd
 
 # Set matplotlib settings
 mpl.get_backend()
@@ -6,13 +7,12 @@ mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-from easyml.datasets import cocaine
 from easyml.factory import easy_glmnet
 
 
 if __name__ == "__main__":
     # Load data
-    data = cocaine.load_data()
+    data = pd.read_table('../data/cocaine.txt')
 
     # Analyze data
     easy_glmnet(data, dependent_variable='DIAGNOSIS', family='binomial', exclude_variables=['subject', 'AGE'])
