01f80ab33ba1669c1818a839756210e4a4de38c5
paulhendricks <paul.hendricks.2013@owu.edu>
2016-11-18 18:20:31 -0500

    some formatting

--- R/easy_glmnet.R
+++ R/easy_glmnet.R
@@ -1,12 +1,15 @@
+# The following information allows one to reproduce the results
+# of the analysis in R.
+# NOTE: easyml may evolve which is why a commit ID is provided.
 # Repository: https://github.com/CCS-Lab/easyml
 # Commit ID:
+# URL: https://github.com/CCS-Lab/easyml/tree/
 library(easyml)
 
 # Load data
 data("cocaine", package = "easyml")
 
 # Analyze data
-easy_glmnet(cocaine, "DIAGNOSIS", family = "binomial",
-            exclude_variables = c("subject"), categorical_variables = "Male",
-            alpha = 1, n_lambda = 200, standardize = False, cut_point = 0,
-            max_iter = 1e6)
+easy_glmnet(cocaine_dependence, "DIAGNOSIS",
+            family = "binomial", exclude_variables = c("subject", "AGE"),
+            categorical_variables = c("Male"))
